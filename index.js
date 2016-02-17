a4756aedc257a0f055049f29798c0b13f5d41d71
Kriegslustig <github@ls7.ch>
2016-02-17 14:12:06 +0100

    Write a nicer implementation of multi-line footnotes

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+'use strict'
+
 const converter = new (require('showdown').Converter)()
 
 module.exports = () => [
   {
     type: 'lang',
     filter: text => text.replace(
-      /^\[\^([\d\w]+)\] (.+(\n(\s{2,}|\t+).+)*)$/m,
-      (str, p1, p2, p3, p4, p5, p6) => `<small class="footnote" id="footnote-${p1}"><a href="#footnote-${p1}"><sup>[${p1}]</sup></a> ${p2.replace(/^\s+/gm, '')}</small>`
+      /^\[\^([\d\w]+)\]( (.+)|(\n+(\s{2,}|\t).+)+)$/mg,
+      (str, p1, p2, p3, p4) => {
+        let content
+        if(p4) {
+          content = converter.makeHtml(p2.replace(/^\s+/gm, ''))
+        } else {
+          content = ' ' + p3
+        }
+        return `<small class="footnote" id="footnote-${p1}"><a href="#footnote-${p1}"><sup>[${p1}]</sup></a>${content}</small>`
+      }
     ),
   },
   {
--- test.js
+++ test.js
@@ -39,8 +39,19 @@ test('note with inner format', t => {
 
 test('note with multi-line notes', t => {
   const testString = [
-    '\n[^1test] The _explanation_.\n\n\n  some more\n  _lines_\n\n',
-    '<p><small class="footnote" id="footnote-1test"><a href="#footnote-1test"><sup>[1test]</sup></a> The <em>explanation</em>.\nsome more\n<em>lines</em></small></p>'
+    '\n[^1test]\n  some more\n  _lines_\n\n',
+    '<p><small class="footnote" id="footnote-1test"><a href="#footnote-1test"><sup>[1test]</sup></a><p>some more\n<em>lines</em></p></small></p>'
+  ]
+  t.same(
+    converter.makeHtml(testString[0]),
+    testString[1]
+  )
+})
+
+test('note with multi-line notes and multiple paragraphs', t => {
+  const testString = [
+    '\n[^1test]\n\n  some more\n  _lines_\n\nyolo\n',
+    '<p><small class="footnote" id="footnote-1test"><a href="#footnote-1test"><sup>[1test]</sup></a><p>some more\n<em>lines</em></p></small></p>\n\n<p>yolo</p>'
   ]
   t.same(
     converter.makeHtml(testString[0]),
