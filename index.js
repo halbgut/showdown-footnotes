5ef280124a5c23bf0e79b2531ae18d25ac0e7cac
Kriegslustig <kriegslustig@kriegslustig.me>
2016-05-25 19:58:45 +0200

    Use the standardjs code style

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ module.exports = () => [
       /^\[\^([\d\w]+)\]:( (.+)|(\n+(\s{2,}|\t).+)+)$/mg,
       (str, p1, p2, p3, p4) => {
         let content
-        if(p4) {
+        if (p4) {
           content = converter.makeHtml(p2.replace(/^\s+/gm, ''))
         } else {
           content = ' ' + p3
         }
         return `<small class="footnote" id="footnote-${p1}"><a href="#footnote-${p1}"><sup>[${p1}]</sup></a>:${content}</small>`
       }
-    ),
+    )
   },
   {
     type: 'lang',
