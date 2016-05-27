4f562e8b09178104218d7dcf17b4edc109468af6
Kriegslustig <kriegslustig@kriegslustig.me>
2016-05-27 14:59:33 +0200

    Implement the #1 enhancment

--- index.js
+++ index.js
@@ -6,16 +6,19 @@ module.exports = () => [
   {
     type: 'lang',
     filter: text => text.replace(
-      /^\[\^([\d\w]+)\]:( ((.+\n)*.+)|\s*(\n+(\s{2,4}|\t).+)+)$/mg,
-      (str, name, rawContent, singleLineContent, _, multilineContent, padding) => {
-        if (multilineContent) {
-          const content = converter.makeHtml(rawContent.replace(new RegExp(`^${padding}`, 'gm'), ''))
-          return `<div class="footnote" id="footnote-${name}"><a href="#footnote-${name}"><sup>[${name}]</sup></a>:${content}</div>`
-        } else {
-          const content = ' ' + singleLineContent
-          return `<small class="footnote" id="footnote-${name}"><a href="#footnote-${name}"><sup>[${name}]</sup></a>:${content}</small>`
-        }
+      /^\[\^([\d\w]+)\]:\s*((\n+(\s{2,4}|\t).+)+)$/mg,
+      (str, name, rawContent, _, padding) => {
+        const content = converter.makeHtml(rawContent.replace(new RegExp(`^${padding}`, 'gm'), ''))
+        return `<div class="footnote" id="footnote-${name}"><a href="#footnote-${name}"><sup>[${name}]</sup></a>:${content}</div>`
       }
+    )
+  },
+  {
+    type: 'lang',
+    filter: text => text.replace(
+      /^\[\^([\d\w]+)\]:( |\n)((.+\n)*.+)$/mg,
+      (str, name, _, content) =>
+        `<small class="footnote" id="footnote-${name}"><a href="#footnote-${name}"><sup>[${name}]</sup></a>: ${content}</small>`
     )
   },
   {
