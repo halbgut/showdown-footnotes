6b5099cb3daa846ffc3f5ab433ee1047d8acbd93
azertypow <azertypow@gmail.com>
2020-08-07 13:36:43 +0200

    feat: add id and anchor for foot note ref in text

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ module.exports = () => [
       /^\[\^([\d\w]+)\]:\s*((\n+(\s{2,4}|\t).+)+)$/mg,
       (str, name, rawContent, _, padding) => {
         const content = converter.makeHtml(rawContent.replace(new RegExp(`^${padding}`, 'gm'), ''))
-        return `<div class="footnote" id="footnote-${name}"><a href="#footnote-${name}"><sup>[${name}]</sup></a>:${content}</div>`
+        return `<div class="footnote" id="footnote-${name}"><a href="#footnote-ref-${name}"><sup>[${name}]</sup></a>:${content}</div>`
       }
     )
   },
@@ -18,14 +18,14 @@ module.exports = () => [
     filter: text => text.replace(
       /^\[\^([\d\w]+)\]:( |\n)((.+\n)*.+)$/mg,
       (str, name, _, content) =>
-        `<small class="footnote" id="footnote-${name}"><a href="#footnote-${name}"><sup>[${name}]</sup></a>: ${content}</small>`
+        `<small class="footnote" id="footnote-${name}"><a href="#footnote-ref-${name}"><sup>[${name}]</sup></a>: ${content}</small>`
     )
   },
   {
     type: 'lang',
     filter: text => text.replace(
       /\[\^([\d\w]+)\]/mg,
-      (str, name) => `<a href="#footnote-${name}"><sup>[${name}]</sup></a>`
+      (str, name) => `<a id="footnote-ref-${name}" href="#footnote-${name}"><sup>[${name}]</sup></a>`
     )
   }
 ]
